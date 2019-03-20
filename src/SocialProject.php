43bcd0dcc9beb7f8eaf5221442ec5769ba8ab549
Hugo <hugosteixeira@hotmail.com>
2019-03-20 20:15:44 -0300

    First commit

---
+++ src/SocialProject.php
@@ -0,0 +1,18 @@
+<?php
+
+class SocialProject{
+    protected $id;
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
